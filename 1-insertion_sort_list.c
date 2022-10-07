790b1376b70a3ba0f08ee0b7389c47e5819d2e87
JasonFlair <emelieobumse100@gmail.com>
2022-10-07 16:42:38 +0100

    progress

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,7 +8,7 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *head, *temp, *temp2, *ptr, *ptr2;
+    listint_t *head, *temp, *ptr, *ptr2;
 
     if (list != NULL)
     {
@@ -18,52 +18,38 @@ void insertion_sort_list(listint_t **list)
         ptr2 = ptr->next;
         ptr2->prev = ptr;
         temp = ptr->prev;
-        temp2 = ptr2->prev;
         
     }
-    while (ptr->next != NULL)
+    while (ptr != NULL)
     {
-        if (ptr->n > ptr2->n)
+        if (ptr->prev != NULL && ptr->prev != head)
         {
-            ptr->next = ptr2->next;
-            ptr2->next->prev = ptr;
-            ptr2->prev = ptr->prev;
-            ptr2->next = ptr;
-            ptr->prev = ptr2;
-            temp->next = ptr2;
-            temp2 = ptr2->prev;
-            temp = ptr->prev;
-            print_list(*list);
-            if (ptr2->prev != NULL && ptr2->prev != head && ptr2->n < temp2->n)
+            if (ptr->n < temp->n)
             {
-                if (ptr2->n < temp2->n)
-                {
-                    ptr2->prev = temp2->prev;
-                    temp2->prev->next = ptr2;
-                    temp2->next = ptr2->next;
-                    ptr2->next->prev = temp2;
-                    ptr2->next = temp2;
-                    temp2->prev = ptr2;
-                    temp2 = ptr2->prev;
-                    temp = ptr->prev;
-                    /* swap*/
-                    print_list(*list);
-                }
-                break;
+                ptr->prev = temp->prev;
+                temp->prev->next = ptr;
+                temp->next = ptr->next;
+                ptr->next->prev = temp;
+                ptr->next = temp;
+                temp->prev = ptr;
+                /* swap done now print */
+                print_list(*list);
+                temp = ptr->prev;
+                /* change temp and check again */
             }
             else
             {
+                ptr = ptr2;
                 ptr2 = ptr->next;
                 temp = ptr->prev;
-                temp2 = ptr2->prev;
             }
+            
         }
         else
         {
             ptr = ptr->next;
             ptr2 = ptr->next;
             temp = ptr->prev;
-            temp2 = ptr2->prev;
         }
     }
 }
