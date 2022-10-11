c741c2935c4d5d99c094e5c016bb800258495067
JasonFlair <emelieobumse100@gmail.com>
2022-10-11 14:40:31 +0100

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -23,7 +23,7 @@ void swap(int *x, int *y)
  * @start: starting position
  * @end: end position 
  * @arr_size: the arr size
- * Return - returns an int 
+ * Return: returns an int 
  */
 
 int partition_index(int *array, int start, int end, size_t arr_size)
